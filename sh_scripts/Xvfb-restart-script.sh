7d3a90bdc2470c0936e03bfa2f253f9046070832
gl_kkebikau <12341234@godeltech.com>
2022-09-21 16:41:05 +0200

    change Jenkinsfile

--- sh_scripts/Xvfb-restart-script.sh
+++ sh_scripts/Xvfb-restart-script.sh
@@ -1,5 +1,5 @@
 echo "restart Xvfb"
-kill -9 `ps aux | grep Xvfb | grep -v grep |  awk '{print $2}'`
+pkill -f Xvfb `ps aux | grep Xvfb | grep -v grep |  awk '{print $2}'`
 sleep 3
 nohup Xvfb :10 -ac > /tmp/Xvfb.log 2>&1 &
 echo "Xvfb started"
